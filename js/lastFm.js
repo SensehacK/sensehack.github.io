72c2a2da4c1ae2109718417860921a7aa8dabfc3
Kautilya <kautilyasave@gmail.com>
2020-03-30 18:38:22 -0400

    (chore)Lint: Last FM image loading bug

--- js/lastFm.js
+++ js/lastFm.js
@@ -15,119 +15,110 @@ var url2 =
   "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=sensehack&limit=5&api_key=4ba03f783090e2ff9ec08e2a63be196b&format=json";
 
 fetch(urlLastFm)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (jsonResponse) {
-        //  console.log(jsonResponse)
-        //  console.log("type of jsonresponse")
-        //  console.log(typeof (jsonResponse))
-        //  console.log("printing try artists")
-        //  console.log(jsonResponse.recenttracks.track[0].artist)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(jsonResponse) {
+    //  console.log(jsonResponse)
+    //  console.log("type of jsonresponse")
+    //  console.log(typeof (jsonResponse))
+    //  console.log("printing try artists")
+    //  console.log(jsonResponse.recenttracks.track[0].artist)
 
-        // Getting nowPlaying from Last FM for first Artist
-        var firstTrack = jsonResponse.recenttracks.track[0]
+    // Getting nowPlaying from Last FM for first Artist
+    var firstTrack = jsonResponse.recenttracks.track[0];
 
-        var nowPlayingB = (firstTrack['@attr']) ? true : false
-        //  console.log("accessing Getting nowPlaying")
-        //  console.log(nowPlayingB)
-        var currentPlayingArtist = firstTrack.artist['#text'];
-        var currentPlayingSong = firstTrack.name;
-        var currentPlayText = "<span>Currently playing: </span>" + currentPlayingArtist + " - " +
-            currentPlayingSong;
-        //  console.log(currentPlayText);
-        // Now playing logic for element hide
-        if (nowPlayingB) {
-            //  console.log("sSensehack")
-            //  console.log(currentPlayText);
-            document.getElementById("currentSong1").hidden = false;
-            document.getElementById("currentSong1").innerHTML =
-                currentPlayText;
+    var nowPlayingB = firstTrack["@attr"] ? true : false;
+    //  console.log("accessing Getting nowPlaying")
+    //  console.log(nowPlayingB)
+    var currentPlayingArtist = firstTrack.artist["#text"];
+    var currentPlayingSong = firstTrack.name;
+    var currentPlayText =
+      "<span>Currently playing: </span>" +
+      currentPlayingArtist +
+      " - " +
+      currentPlayingSong;
+    //  console.log(currentPlayText);
+    // Now playing logic for element hide
+    if (nowPlayingB) {
+      //  console.log("sSensehack")
+      //  console.log(currentPlayText);
+      document.getElementById("currentSong1").hidden = false;
+      document.getElementById("currentSong1").innerHTML = currentPlayText;
+    } else {
+      //  console.log("else sensehack");
+      document.getElementById("currentSong1").hidden = true;
+      document.getElementById("songGifPlay").hidden = true;
+    }
 
-        } else {
-            //  console.log("else sensehack");
-            document.getElementById("currentSong1").hidden = true;
-            document.getElementById("songGifPlay").hidden = true;
+    //Accessing 8 json elements overall
+    for (var i = 0; i < 8; i++) {
+      // Getting accessing artists from Last FM
+      var track = jsonResponse.recenttracks.track[i];
 
-        }
+      //  console.log("printing url 2 data ")
+      //  console.log(track)
 
+      // Getting accessing artists from Last FM
+      var artistName = track.artist["#text"];
+      //  console.log("accessing artists")
+      //  console.log(artistName)
 
-        //Accessing 8 json elements overall
-        for (var i = 0; i < 8; i++) {
+      // Getting accessing artistSongName from Last FM
+      var artistSongName = track.name;
+      //  console.log("accessing artistSongName")
+      //  console.log(artistSongName)
 
-            // Getting accessing artists from Last FM
-            var track = jsonResponse.recenttracks.track[i]
+      // Getting image[3] for highest quality to showcase
+      var artistNameSongArr = track.image[3];
+      //  console.log("accessing artistNameSongImage")
+      var artistNameSongImage = artistNameSongArr["#text"];
+      //  console.log(artistNameSongImage)
+      //  console.log(i + 1)
 
-            //  console.log("printing url 2 data ")
-            //  console.log(track)
+      // Setting variable for dynamic ID generation
+      // Setting the ID with artistName + (i + 1) Because I named them wrong at starting
+      var artistSongNameArr = "albumSong" + (i + 1);
+      // Setting the ID with artistName + (i + 1) Because I named them wrong at starting
+      var artistNameArr = "albumArtist" + (i + 1);
+      // Setting the ID with albumSongImage + (i + 1) Because I named them wrong at starting
+      var artistSongNameImageArr = "albumSongImage" + (i + 1);
+      var artistSongNameBImageArr = "albumSongBImage" + (i + 1);
+      //Debug prints regarding numbering system. Turns out my 8 elements logic < 7 for i = 0 was missing out for element 8th in JSON response.
+      //  console.log(artistSongNameArr)
+      //  console.log(artistNameArr)
+      //  console.log(artistSongNameImageArr)
 
-            // Getting accessing artists from Last FM
-            var artistName = track.artist['#text']
-            //  console.log("accessing artists")
-            //  console.log(artistName)
+      //Debug Prints Logs
+      //  console.log("var artistSongNameImageArr = artistNameSongImage + i ")
+      //  console.log(artistSongNameImageArr)
 
-            // Getting accessing artistSongName from Last FM
-            var artistSongName = track.name
-            //  console.log("accessing artistSongName")
-            //  console.log(artistSongName)
+      // Setting songs meta data on HTML by document by get Element ID
+      document.getElementById(artistNameArr).innerHTML = artistName;
+      document.getElementById(artistSongNameArr).innerHTML = artistSongName;
 
+      // Getting nowPlaying from Last FM
+      var artistArtworkSongB = artistNameSongImage ? true : false;
 
-            // Getting image[3] for highest quality to showcase
-            var artistNameSongArr = track.image[3]
-            //  console.log("accessing artistNameSongImage")
-            var artistNameSongImage = artistNameSongArr['#text']
-            //  console.log(artistNameSongImage)
-            //  console.log(i + 1)
+      // Now Image boxes with Last Fm  Music Scrobble Images or don't set images artwork.
+      if (artistArtworkSongB) {
+        //  console.log("artistArtworkSongB artwork found")
+        //  console.log(artistArtworkSongB)
+        //Setting main 8 blocks image
+        document.getElementById(
+          artistSongNameImageArr
+        ).src = artistNameSongImage;
 
-
-            // Setting variable for dynamic ID generation 
-            // Setting the ID with artistName + (i + 1) Because I named them wrong at starting
-            var artistSongNameArr = "albumSong" + (i + 1)
-            // Setting the ID with artistName + (i + 1) Because I named them wrong at starting
-            var artistNameArr = "albumArtist" + (i + 1)
-            // Setting the ID with albumSongImage + (i + 1) Because I named them wrong at starting
-            var artistSongNameImageArr = "albumSongImage" + (i + 1)
-            var artistSongNameBImageArr = "albumSongBImage" + (i + 1)
-            //Debug prints regarding numbering system. Turns out my 8 elements logic < 7 for i = 0 was missing out for element 8th in JSON response.
-            //  console.log(artistSongNameArr)
-            //  console.log(artistNameArr)
-            //  console.log(artistSongNameImageArr)
-
-            //Debug Prints Logs
-            //  console.log("var artistSongNameImageArr = artistNameSongImage + i ")
-            //  console.log(artistSongNameImageArr)
-
-            // Setting songs meta data on HTML by document by get Element ID
-            document.getElementById(artistNameArr).innerHTML = artistName
-            document.getElementById(artistSongNameArr).innerHTML = artistSongName
-
-
-            // Getting nowPlaying from Last FM
-            var artistArtworkSongB = (artistNameSongImage) ? true : false
-
-            // Now Image boxes with Last Fm  Music Scrobble Images or don't set images artwork.
-            if (artistArtworkSongB) {
-                //  console.log("artistArtworkSongB artwork found")
-                //  console.log(artistArtworkSongB)
-                //Setting main 8 blocks image
-                document.getElementById(artistSongNameImageArr).src =
-                    artistNameSongImage;
-                //Setting pop up modal image
-                document.getElementById(artistSongNameBImageArr).src =
-                    artistNameSongImage;
-
-            } else {
-                //  console.log("No artistArtworkSongB artwork found")
-            }
-
-        }
-
-        /*
         // Commenting below code as we have removed the modal completely.
         //Setting pop up modal image
         // document.getElementById(
         //   artistSongNameBImageArr
         // ).src = artistNameSongImage;
+        //  console.log("No artistArtworkSongB artwork found")
+      }
+    }
+
+    /*
         //Commenting them as I have removed this div tags.
         // Setting once out of loop.
         document.getElementById("artistNameP1").innerHTML = artistName
--- js/rescueTime.js
+++ js/rescueTime.js
@@ -12,44 +12,47 @@ var rTdistracting_percentage;
 var rTvery_distracting_percentage;
 
 function movierescueTime() {
-    var request = new XMLHttpRequest();
-    var getRescueTime =
-        "https://www.rescuetime.com/anapi/daily_summary_feed?key=B63TFb2EXGglprpQqYrz4zIgChijDRfsZaddbjEN";
+  var request = new XMLHttpRequest();
+  var getRescueTime =
+    "https://www.rescuetime.com/anapi/daily_summary_feed?key=B63TFb2EXGglprpQqYrz4zIgChijDRfsZaddbjEN";
 
-    var getRescueTimeX =
-        "https://cors.io/?https://www.rescuetime.com/anapi/daily_summary_feed?key=B63TFb2EXGglprpQqYrz4zIgChijDRfsZaddbjEN";
+  var getRescueTimeX =
+    "https://cors.io/?https://www.rescuetime.com/anapi/daily_summary_feed?key=B63TFb2EXGglprpQqYrz4zIgChijDRfsZaddbjEN";
 
-    // call url 
-    request.open('GET', getRescueTimeX);
-    request.onreadystatechange = function () {
-        if (this.readyState === 4) {
-            this.getAllResponseHeaders();
-            //Converting responseText String JSON to Javascript Object JSON.
-            var rescueTimeJSON = this.responseText;
-            var rescueTimeObj = JSON.parse(rescueTimeJSON);
-            // Accessing 8 json elements overall
-            var rescueTimePulse = rescueTimeObj[0].productivity_pulse;
+  // call url
+  request.open("GET", getRescueTime);
+  request.onreadystatechange = function() {
+    if (this.readyState === 4) {
+      this.getAllResponseHeaders();
+      //Converting responseText String JSON to Javascript Object JSON.
+      var rescueTimeJSON = this.responseText;
+      var rescueTimeObj = JSON.parse(rescueTimeJSON);
+      // Accessing 8 json elements overall
+      var rescueTimePulse = rescueTimeObj[0].productivity_pulse;
 
-            // Getting the first time analysis for yesterday.
-            rTvery_productive_percentage = Math.floor(rescueTimeObj[0].very_productive_percentage);
-            rTproductive_percentage = Math.floor(rescueTimeObj[0].productive_percentage);
-            rTneutral_percentage = Math.floor(rescueTimeObj[0].neutral_percentage);
-            rTdistracting_percentage = Math.floor(rescueTimeObj[0].distracting_percentage);
-            rTvery_distracting_percentage = Math.floor(rescueTimeObj[0].very_distracting_percentage);
+      // Getting the first time analysis for yesterday.
+      rTvery_productive_percentage = Math.floor(
+        rescueTimeObj[0].very_productive_percentage
+      );
+      rTproductive_percentage = Math.floor(
+        rescueTimeObj[0].productive_percentage
+      );
+      rTneutral_percentage = Math.floor(rescueTimeObj[0].neutral_percentage);
+      rTdistracting_percentage = Math.floor(
+        rescueTimeObj[0].distracting_percentage
+      );
+      rTvery_distracting_percentage = Math.floor(
+        rescueTimeObj[0].very_distracting_percentage
+      );
+    }
+  };
 
-        }
-
-    };
-
-    request.send();
-
+  request.send();
 }
 
-
-
 // Load the Visualization API and the core chart package.
-google.charts.load('current', {
-    'packages': ['corechart']
+google.charts.load("current", {
+  packages: ["corechart"]
 });
 
 // Set a callback to run when the Google Visualization API is loaded.
@@ -58,60 +61,59 @@ google.charts.setOnLoadCallback(rescueTimeChart);
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 
-
 function rescueTimeChart() {
-    var rescueData2 = new google.visualization.DataTable();
-    rescueData2.addColumn('string', 'Topping');
-    rescueData2.addColumn('number', 'Slices');
-    // rescueData2.addColumn('number', 'Percentage');
+  var rescueData2 = new google.visualization.DataTable();
+  rescueData2.addColumn("string", "Topping");
+  rescueData2.addColumn("number", "Slices");
+  // rescueData2.addColumn('number', 'Percentage');
 
-    rescueData2.addRows([
-        ['Very Productive', rTvery_productive_percentage],
-        ['Productive', rTproductive_percentage],
-        ['Neutral', rTneutral_percentage],
-        ['Distracting', rTdistracting_percentage],
-        ['Very Distracting', rTvery_distracting_percentage]
-    ]);
-    //  console.log("In drawChart3 google 23 pie chart");
-    var rescueTimeOptions = {
-        'title': 'How Much Time I utilize yesterday.',
-        'height': 400
-        // 'width': 600,
+  rescueData2.addRows([
+    ["Very Productive", rTvery_productive_percentage],
+    ["Productive", rTproductive_percentage],
+    ["Neutral", rTneutral_percentage],
+    ["Distracting", rTdistracting_percentage],
+    ["Very Distracting", rTvery_distracting_percentage]
+  ]);
+  //  console.log("In drawChart3 google 23 pie chart");
+  var rescueTimeOptions = {
+    title: "How Much Time I utilize yesterday.",
+    height: 400
+    // 'width': 600,
+  };
 
-    };
+  var rescueTimeChart2 = new google.visualization.PieChart(
+    document.getElementById("rescueTimeChartPie_div3")
+  );
+  rescueTimeChart2.draw(rescueData2, rescueTimeOptions);
 
-    var rescueTimeChart2 = new google.visualization.PieChart(document.getElementById(
-        'rescueTimeChartPie_div3'));
-    rescueTimeChart2.draw(rescueData2, rescueTimeOptions);
-
-    //Call the bar Function on load.
-    rescueTimeChartBar();
+  //Call the bar Function on load.
+  rescueTimeChartBar();
 }
 
 function rescueTimeChartBar() {
-    var rescueDataBar2 = new google.visualization.DataTable();
-    rescueDataBar2.addColumn('string', 'Topping');
-    rescueDataBar2.addColumn('number', 'Percent');
-    // rescueData2.addColumn('number', 'Percentage');
+  var rescueDataBar2 = new google.visualization.DataTable();
+  rescueDataBar2.addColumn("string", "Topping");
+  rescueDataBar2.addColumn("number", "Percent");
+  // rescueData2.addColumn('number', 'Percentage');
 
-    rescueDataBar2.addRows([
-        ['Very Productive', rTvery_productive_percentage],
-        ['Productive', rTproductive_percentage],
-        ['Neutral', rTneutral_percentage],
-        ['Distracting', rTdistracting_percentage],
-        ['Very Distracting', rTvery_distracting_percentage]
-    ]);
-    //  console.log("In drawChart3 google 23 bar chart");
-    var rescueTimeBarOptions = {
-        'title': 'How Much Time I utilize yesterday.',
-        'height': 400
-        // 'width': 600,
+  rescueDataBar2.addRows([
+    ["Very Productive", rTvery_productive_percentage],
+    ["Productive", rTproductive_percentage],
+    ["Neutral", rTneutral_percentage],
+    ["Distracting", rTdistracting_percentage],
+    ["Very Distracting", rTvery_distracting_percentage]
+  ]);
+  //  console.log("In drawChart3 google 23 bar chart");
+  var rescueTimeBarOptions = {
+    title: "How Much Time I utilize yesterday.",
+    height: 400
+    // 'width': 600,
+  };
 
-    };
-
-    var rescueTimeChartBar2 = new google.visualization.BarChart(document.getElementById(
-        'rescueTimeChartBar_div3'));
-    rescueTimeChartBar2.draw(rescueDataBar2, rescueTimeBarOptions);
+  var rescueTimeChartBar2 = new google.visualization.BarChart(
+    document.getElementById("rescueTimeChartBar_div3")
+  );
+  rescueTimeChartBar2.draw(rescueDataBar2, rescueTimeBarOptions);
 }
 
 //  </script>
