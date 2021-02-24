b2d8fe356eafb3c19aa702f273098be496eaf3a1
Kautilya <kautilyasave@gmail.com>
2021-02-24 15:08:17 -0600

    (fix)RescueTime: Null handling

--- js/rescueTime.js
+++ js/rescueTime.js
@@ -21,29 +21,31 @@ function movierescueTime() {
 
   // call url
   request.open("GET", getRescueTime);
-  request.onreadystatechange = function() {
+  request.onreadystatechange = function () {
     if (this.readyState === 4) {
       this.getAllResponseHeaders();
       //Converting responseText String JSON to Javascript Object JSON.
       var rescueTimeJSON = this.responseText;
-      var rescueTimeObj = JSON.parse(rescueTimeJSON);
-      // Accessing 8 json elements overall
-      var rescueTimePulse = rescueTimeObj[0].productivity_pulse;
+      if (rescueTimeJSON) {
+        var rescueTimeObj = JSON.parse(rescueTimeJSON);
+        // Accessing 8 json elements overall
+        var rescueTimePulse = rescueTimeObj[0].productivity_pulse;
 
-      // Getting the first time analysis for yesterday.
-      rTvery_productive_percentage = Math.floor(
-        rescueTimeObj[0].very_productive_percentage
-      );
-      rTproductive_percentage = Math.floor(
-        rescueTimeObj[0].productive_percentage
-      );
-      rTneutral_percentage = Math.floor(rescueTimeObj[0].neutral_percentage);
-      rTdistracting_percentage = Math.floor(
-        rescueTimeObj[0].distracting_percentage
-      );
-      rTvery_distracting_percentage = Math.floor(
-        rescueTimeObj[0].very_distracting_percentage
-      );
+        // Getting the first time analysis for yesterday.
+        rTvery_productive_percentage = Math.floor(
+          rescueTimeObj[0].very_productive_percentage,
+        );
+        rTproductive_percentage = Math.floor(
+          rescueTimeObj[0].productive_percentage,
+        );
+        rTneutral_percentage = Math.floor(rescueTimeObj[0].neutral_percentage);
+        rTdistracting_percentage = Math.floor(
+          rescueTimeObj[0].distracting_percentage,
+        );
+        rTvery_distracting_percentage = Math.floor(
+          rescueTimeObj[0].very_distracting_percentage,
+        );
+      }
     }
   };
 
@@ -52,7 +54,7 @@ function movierescueTime() {
 
 // Load the Visualization API and the core chart package.
 google.charts.load("current", {
-  packages: ["corechart"]
+  packages: ["corechart"],
 });
 
 // Set a callback to run when the Google Visualization API is loaded.
@@ -72,17 +74,17 @@ function rescueTimeChart() {
     ["Productive", rTproductive_percentage],
     ["Neutral", rTneutral_percentage],
     ["Distracting", rTdistracting_percentage],
-    ["Very Distracting", rTvery_distracting_percentage]
+    ["Very Distracting", rTvery_distracting_percentage],
   ]);
   //  console.log("In drawChart3 google 23 pie chart");
   var rescueTimeOptions = {
     title: "How Much Time I utilize yesterday.",
-    height: 400
+    height: 400,
     // 'width': 600,
   };
 
   var rescueTimeChart2 = new google.visualization.PieChart(
-    document.getElementById("rescueTimeChartPie_div3")
+    document.getElementById("rescueTimeChartPie_div3"),
   );
   rescueTimeChart2.draw(rescueData2, rescueTimeOptions);
 
@@ -101,17 +103,17 @@ function rescueTimeChartBar() {
     ["Productive", rTproductive_percentage],
     ["Neutral", rTneutral_percentage],
     ["Distracting", rTdistracting_percentage],
-    ["Very Distracting", rTvery_distracting_percentage]
+    ["Very Distracting", rTvery_distracting_percentage],
   ]);
   //  console.log("In drawChart3 google 23 bar chart");
   var rescueTimeBarOptions = {
     title: "How Much Time I utilize yesterday.",
-    height: 400
+    height: 400,
     // 'width': 600,
   };
 
   var rescueTimeChartBar2 = new google.visualization.BarChart(
-    document.getElementById("rescueTimeChartBar_div3")
+    document.getElementById("rescueTimeChartBar_div3"),
   );
   rescueTimeChartBar2.draw(rescueDataBar2, rescueTimeBarOptions);
 }
